a9200e89a299d4f3a6a6eb028fbdb318269ff679
Luvicka <lucje.vildomcova@gmail.com>
2025-02-26 20:58:50 +0100

    Comit xpath

---
+++ cypress/e2e/learning/xpath/xpath_login.cy.js
@@ -0,0 +1,11 @@
+describe("Learning XPath", () => {
+  it("Login with Xpath Selectors", () => {
+    cy.visit('https://tredgate.com/pmtool')
+    cy.xpath("//input[@id='username']").type("cypress_zima_2024");
+    cy.xpath("//input [@id='password']").type("Zima2024Cypress");
+    cy.xpath("//button [@type='submit']").click();
+      cy.xpath("//i[@class='fa fa-angle-down']").click();
+    cy.xpath("//i[@class='fa fa-sign-out']").click();
+
+  });
+});
